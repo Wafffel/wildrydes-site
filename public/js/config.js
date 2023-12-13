df115cf5e6ed2ac50ec51ba9db065a0f22c324b3
Wafffel <91781571+Wafffel@users.noreply.github.com>
2023-12-13 09:45:51 +0100

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_Qh48UIZ1J', // e.g. eu-east-2_uXboG5pAb
-        userPoolClientId: '15hhvh7cnct9imuatln22p82hr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-west-1_dg6Fkdrs8', // e.g. eu-east-2_uXboG5pAb
+        userPoolClientId: 'p4lu32v957qdnv4sqaibii0v7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
